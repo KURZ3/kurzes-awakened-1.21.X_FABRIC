911043ee91437b799bca331c6427cd900a069c91
Kurze <Marcelo_andre2003@ghotmail.com>
2025-02-07 02:14:05 -0600

    Created the class for the mod items

---
+++ src/main/java/com/kurzesawakened/item/ModItems.java
@@ -0,0 +1,41 @@
+package com.kurzesawakened.item;
+
+import com.kurzesawakened.KurzesAwakened;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    //------------------------------ITEMS---------------------
+    public static final Item KURZARITE_CLUSTER = registerItem("kurzarite_cluster", new Item(new Item.Settings()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //------------------------------METHODS---------------------
+    private static Item registerItem(String name, Item item){ //Builder preset used for out items
+        return Registry.register(Registries.ITEM, Identifier.of(KurzesAwakened.MOD_ID, name), item);
+    }
+    public static void registerModItems(){ //Method used for registering our items
+        KurzesAwakened.LOGGER.info("Registering Mod Items for" + KurzesAwakened.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(KURZARITE_CLUSTER);
+        });
+    }
+}
