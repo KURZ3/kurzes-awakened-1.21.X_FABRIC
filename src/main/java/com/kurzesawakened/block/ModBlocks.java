3c21229bc43b41989d825c0a6e36177550c81739
Kurze <Marcelo_andre2003@ghotmail.com>
2025-02-12 15:35:36 -0600

    Added the first block :)

---
+++ src/main/java/com/kurzesawakened/block/ModBlocks.java
@@ -0,0 +1,50 @@
+package com.kurzesawakened.block;
+
+import com.kurzesawakened.KurzesAwakened;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    //------------------------------BLOCKS----------------------------------
+    public static final Block SCULK_DECO = registerBlock("sculk_deco",
+            new Block(AbstractBlock.Settings.create().strength(5f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+
+
+
+
+
+
+
+
+
+
+    //------------------------------REGISTERING METHODS---------------------
+
+    private static Block registerBlock(String name, Block block){ //Method used for creating and registering the block
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(KurzesAwakened.MOD_ID,name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block){ //Method used for assigning a BlockItem to the registered block
+        Registry.register(Registries.ITEM, Identifier.of(KurzesAwakened.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBLocks(){
+        KurzesAwakened.LOGGER.info("Registering Mod Blocks for " + KurzesAwakened.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries->{ //We add the block to the Building Blocks Tab
+            entries.add(ModBlocks.SCULK_DECO);
+        });
+    }
+}
